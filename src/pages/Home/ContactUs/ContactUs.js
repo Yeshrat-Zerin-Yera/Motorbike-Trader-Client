0ec70e792022c55e607b4c0c46c5a73dd3a33ffa
unknown <yeshratzerinyera@gmail.com>
2023-01-05 10:59:28 +0600

    Connect Us Added

---
+++ src/pages/Home/ContactUs/ContactUs.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import ContactUsImg from '../../../assets/images/contact-us.png';
+
+const ContactUs = () => {
+    // Set Subject
+    const [subject, setSubject] = useState('')
+    // Set Message
+    const [message, setMessage] = useState('');
+
+    const handleGetMailDetails = event => {
+        const form = event.target;
+        if (form.name === 'subject') {
+            setSubject(form.value);
+        }
+        setMessage(form.value);
+    };
+
+    return (
+        <div>
+            {/* Form */}
+            <form onChange={handleGetMailDetails} method="post" action={`mailto:yeshratzerinyera@gmail.com?subject=${subject}&body=${message}`} className='flex items-center flex-col gap-3 p-6 mt-12' style={{ backgroundImage: `url(${ContactUsImg})`, backgroundRepeat: 'no-repeat', backgroundSize: 'cover' }}>
+                {/* Contact Us Title */}
+                <h4 className="text-2xl font-bold text-center text-white">Contact Us</h4>
+                {/* Subject */}
+                <input type="text" name='subject' placeholder="Subject" className="input input-bordered w-full max-w-xs" />
+                {/* Message */}
+                <textarea className="textarea input-bordered w-full max-w-xs text-[16px]" name='message' placeholder="Message"></textarea>
+                {/* Submit Button */}
+                <button className="btn border-0 bg-gradient-to-r from-cyan-400 to-blue-500 w-full max-w-xs">Send</button>
+                {/* Reset Button */}
+                <button type='reset' className="btn btn-outline border-2 border-blue-500 text-blue-500 w-full max-w-xs">Reset</button>
+            </form>
+        </div>
+    );
+};
+
+export default ContactUs;
--- src/pages/Home/Home/Home.js
+++ src/pages/Home/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import BannerCarousel from '../BannerCarousel/BannerCarousel';
+import ContactUs from '../ContactUs/ContactUs';
 
 const Home = () => {
     return (
         <div className='my-12'>
             <BannerCarousel></BannerCarousel>
+            <ContactUs></ContactUs>
         </div>
     );
 };
